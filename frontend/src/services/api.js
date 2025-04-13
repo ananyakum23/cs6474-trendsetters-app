d6c39dad833963a3e22ec294f7408ec70f32c9ba
athodpunuri3 <amogha.tho@gmail.com>
2025-04-13 10:41:41 -0400

    fix top posts api call

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,10 +4,15 @@ const BASE_URL = 'http://127.0.0.1:5000';
 // const BASE_URL = 'https://cs6474-trendsetters-app.onrender.com';
 
 
-export const fetchTopPosts = () => axios.get(`${BASE_URL}/top-engagement`);
+// export const fetchTopPosts = () => axios.get(`${BASE_URL}/top-engagement`);
 // export const fetchClusterNames = () => axios.get(`${BASE_URL}/cluster-names`);
 // export const fetchForecastForCluster = (id) =>
 //   axios.get(`${BASE_URL}/forecast-multi/${id}`);
+
+export const fetchTopPosts = (subreddit) =>
+  axios.get(`${BASE_URL}/top-engagement`, {
+    params: { subreddit }
+  });
 
 export const fetchClusterNames = (subreddit) =>
   axios.get(`${BASE_URL}/cluster-names`, {
